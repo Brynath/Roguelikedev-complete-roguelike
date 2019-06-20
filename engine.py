85ffbb819e2718e5d6935fc3092186b4c2cdcbe4
Daniel Allen <dallen@spbhs.org>
2019-06-19 21:16:08 -0800

    Tutorial section 1 is done... finally.
    I did find an issue with the tutorial where there was supposed to be a spot where the '@' was to be deleted earlier, but there wasn't any mention of it in the tutorial.

--- engine.py
+++ engine.py
@@ -13,6 +13,8 @@ def main():
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
     libtcod.console_init_root(screen_width, screen_height, 'Working Title', False)
+
+    con = libtcod.console_new(screen_width, screen_height)
     
     key = libtcod.Key()
     mouse = libtcod.Mouse()
@@ -20,11 +22,15 @@ def main():
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
-        libtcod.console_set_default_foreground(0, libtcod.white)
-        libtcod.console_put_char(0, player_x, player_y, '@', libtcod.BKGND_NONE)
+        libtcod.console_set_default_foreground(con, libtcod.white)
+        libtcod.console_put_char(con, player_x, player_y, '@', libtcod.BKGND_NONE)
+        libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
+       # libtcod.console_set_default_foreground(0, libtcod.white)
+       # libtcod.console_put_char(0, player_x, player_y, '@', libtcod.BKGND_NONE)
         libtcod.console_flush()
 
-       # key = libtcod.console_check_for_keypress()
+        libtcod.console_put_char(con, player_x, player_y, ' ', libtcod.BKGND_NONE)
+
         action = handle_keys(key)
 
         move = action.get('move')
@@ -36,7 +42,6 @@ def main():
             player_x += dx
             player_y += dy
 
-        #if key.vk == libtcod.KEY_ESCAPE:
         if exit:    
             return True
 
