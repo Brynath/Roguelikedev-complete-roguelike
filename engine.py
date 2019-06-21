529da2094ca6da210ebdd603a2e3f2d714f6f015
Daniel Allen <dallen@spbhs.org>
2019-06-20 16:37:49 -0800

    Started Section 2 Renamed the game Squeeek!

--- engine.py
+++ engine.py
@@ -1,18 +1,24 @@
 import tcod as libtcod
 
+from entity import Entity
 from input_handlers import handle_keys
+from render_functions import clear_all, render_all
 
 
 def main():
     screen_width = 80
     screen_height = 50
 
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
+    #player_x = int(screen_width // 2)
+    #player_y = int(screen_height // 2)
+
+    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
+    npc = Entity(int(screen_width /2 - 5), int(screen_height / 2 + 1), '@', libtcod.amber)
+    entities = [npc, player]
 
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
-    libtcod.console_init_root(screen_width, screen_height, 'Working Title', False)
+    libtcod.console_init_root(screen_width, screen_height, 'Squeeek!', False)
 
     con = libtcod.console_new(screen_width, screen_height)
     
@@ -22,14 +28,10 @@ def main():
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
-        libtcod.console_set_default_foreground(con, libtcod.white)
-        libtcod.console_put_char(con, player_x, player_y, '@', libtcod.BKGND_NONE)
-        libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
-       # libtcod.console_set_default_foreground(0, libtcod.white)
-       # libtcod.console_put_char(0, player_x, player_y, '@', libtcod.BKGND_NONE)
+        render_all(con, entities, screen_width, screen_height)
         libtcod.console_flush()
 
-        libtcod.console_put_char(con, player_x, player_y, ' ', libtcod.BKGND_NONE)
+        clear_all(con, entities)
 
         action = handle_keys(key)
 
@@ -39,8 +41,9 @@ def main():
 
         if move:
             dx, dy = move
-            player_x += dx
-            player_y += dy
+            #player_x += dx
+            #player_y += dy
+            player.move(dx, dy)
 
         if exit:    
             return True
